bbf134cff52c930841697c264a6d04c5c0bd8fc1
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-08-18 19:14:12 +0300

    Issue #1: Add a user creation page

--- fpq/fpq_user/forms.py
+++ fpq/fpq_user/forms.py
@@ -1,24 +1,55 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import CharField, PasswordInput, TextInput
+from django.forms import CharField, EmailField, EmailInput, PasswordInput, \
+    TextInput
 
 
-ATTRIBUTES = {'class': 'form-control'}
+USERNAME = {'min_length': 3, 'max_length': 16, 'required': True}
 
 
-class FpqUserAuthenticationForm(AuthenticationForm):
+def attributes(identifier: str) -> dict:
+    if not identifier:
+        raise ValueError('The tag identifier can not be empty.')
+
+    return {'id': identifier, 'class': 'form-control'}
+
+
+class LoginForm(AuthenticationForm):
     username = CharField(
-        min_length=3,
-        max_length=16,
-        required=True,
-        widget=TextInput({'id': 'username', **ATTRIBUTES}),
+        widget=TextInput(attributes('username')),
+        **USERNAME,
     )
 
     password = CharField(
         required=True,
-        widget=PasswordInput({'id': 'password', **ATTRIBUTES}),
+        widget=PasswordInput(attributes('password')),
     )
 
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class RegisterForm(UserCreationForm):
+    username = CharField(widget=TextInput(attributes('username')), **USERNAME)
+
+    email = EmailField(
+        min_length=7,
+        max_length=40,
+        required=True,
+        widget=EmailInput(attributes('email')),
+    )
+
+    password1 = CharField(
+        required=True,
+        widget=PasswordInput(attributes('password')),
+    )
+
+    password2 = CharField(
+        required=True,
+        widget=PasswordInput(attributes('confirm-password')),
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- fpq/fpq_user/views.py
+++ fpq/fpq_user/views.py
@@ -1,9 +1,39 @@
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponsePermanentRedirect
-from django.shortcuts import redirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect
+from django.shortcuts import redirect, render
+from django.views import View
+
+from .forms import RegisterForm
 
 
+Response = HttpResponse | HttpResponsePermanentRedirect
+
+
+class RegisterView(View):
+    template_name = 'fpq_user/register.html'
+    form_class = RegisterForm
+
+    def dispatch(self, request: WSGIRequest) -> Response:
+        return super().dispatch(request) if request.user.is_anonymous else \
+            redirect('fpq_core:index')
+
+    def get(self, request: WSGIRequest) -> HttpResponse:
+        return render(request, self.template_name, {'form': self.form_class})
+
+    def post(self, request: WSGIRequest) -> Response:
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('fpq_core:index')
+
+        return render(request, self.template_name, {'form': form})
+
+
+@login_required
 def signout(request: WSGIRequest) -> HttpResponsePermanentRedirect:
     logout(request)
 
